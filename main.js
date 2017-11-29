50fab1a1a00fd3a42a04e910cf90361b21c1fdef
ortaly <ortalronen1@gmail.com>
2017-11-29 09:31:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,22 +6,3 @@ const listing = require('./src/listing');
 
 
 listing.search();
-
-
-// reviewsParams = new URLSearchParams([
-//     ['client_id', '3092nxybyb0otqw18e8nh5nty'],
-//     ['role', 'all'],
-//     ['listing_id', '13465986']
-//   ]);
-
-// fetch(`https://api.airbnb.com/v2/reviews?${reviewsParams.toString()}`, { agent:new HttpsProxyAgent('http://genproxy:8080')})
-// .then(function(res) {
-//     return res.json();
-// }).then(body =>
-//     fs.writeFile('../AirBNB-Node.JS-challenge/review.txt', JSON.stringify(body), function(err){
-//         if(err){
-//             return console.log("err=" + err);
-//         }
-//     })
-//     // console.log(res);
-// );
